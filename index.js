1bc7f2b985886c744c44e2e547c552f1b4a41942
ihor-pyvovarnyk <igor.pivovarnik@gmail.com>
2015-03-29 02:24:45 +0200

    reorganised callbacks sequence modified messages, which logs into console and sends to gitter

--- index.js
+++ index.js
@@ -8,30 +8,24 @@ var messageTemplate = /^calc[\s]+([0-9\(\)\+\-\*\/\s)]+)$/i;
 var gitter = new Gitter(token);
 gitter.currentUser()
 .then(function (user) {
-    console.log('Logged as ' + user.username);
-    gitter.rooms.join(roomUri)
-    .then(function(room) {
-        console.log('Joined room: ', room.name);
-        var events = room.listen();
-        events.on('message', function (message) {
-            var msg = message.text;
-            console.log('New message: ' + msg);
-            if (messageTemplate.test(msg)) {
-                var exp = messageTemplate.exec(msg)[1] || '';
-                var resMsg = '';
-                try {
-                    resMsg = exp + ' = ' + eval(exp);
-                } catch (e) {
-                    resMsg = 'Invalid calc expression';
-                }
-                console.log(resMsg);
-                room.send(resMsg);
-            } else {
-                console.log('Message don\'t match calc template');
+    console.log('Logged as', user.username);
+    return gitter.rooms.join(roomUri)
+})
+.then(function(room) {
+    console.log('Joined room:', room.name);
+    room.listen().on('message', function (message) {
+        var msg = message.text;
+        console.log('New message:', msg);
+        if (messageTemplate.test(msg)) {
+            var exp = messageTemplate.exec(msg)[1] || '';
+            try {
+                room.send(exp + ' = ' + eval(exp));
+            } catch (e) {
+                room.send('Invalid calc expression');
             }
-        });
-    })
-    .fail(function(err) {
-        console.log('Not possible to join the room: ', err);
+        }
     });
+})
+.fail(function(err) {
+    console.log('Cannot join the room');
 });
